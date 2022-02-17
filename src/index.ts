290455d4c9943429fe02d3ff089a0d0125c6ce91
Tristan <LeoDog896@hotmail.com>
2022-02-16 20:31:02 -0500

    Begin base PWA plugin

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import type { Plugin } from "rollup"
+
+export default function pwa(): Plugin {
+    return {
+        name: 'pwa', // this name will show up in warnings and errors
+        resolveId(source) {
+            if (source === 'virtual-module') {
+                return source; // this signals that rollup should not ask other plugins or check the file system to find this id
+            }
+            return null; // other ids should be handled as usually
+        },
+        load(id) {
+            if (id === 'virtual-module') {
+                return 'export default "This is virtual!"'; // the source code for "virtual-module"
+            }
+            return null; // other ids should be handled as usually
+        }
+    };
+}
+  
